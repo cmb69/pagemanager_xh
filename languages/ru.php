378c56890e3412c88c4670bbee4a6e5805355db3
cmb69 <cmbecker69@gmx.de>
2012-09-20 16:40:39 +0000

    updated Russian language file (contributed by old)

--- languages/ru.php
+++ languages/ru.php
@@ -1,46 +1,54 @@
 <?php
 
-$plugin_tx['pagemanager']['cf_verbose']="<p>Показать информацию и подтверждения диалогов? <b>true</b> или <b>false</b></p><p><strong><b>true</b> настоятельно рекомендуется</strong>, если вы не опытный пользователь <strong>и</strong> привыкли к Pagemanager_XH.</p>";
-$plugin_tx['pagemanager']['cf_toolbar_show']="<p>Панели инструментов могут быть показаны? <b>true</b> или <b>false</b></p>";
-$plugin_tx['pagemanager']['cf_toolbar_vertical']="<p>Если панель инструментов отображается вертикально? <b>true</b> or <b>false</b></p><p>Эта опция предназначена для опытных пользователей, которые могут настроить макет плагина в Стилях.</p>";
-$plugin_tx['pagemanager']['cf_pagedata_attribute']="<p>Да/нет (по умолчанию да) pagedata атрибут для галочки, например. <b>published</b> или <b>linked_to_menu</b>.</p><p><strong>Внимание:</strong> Если вы выбрали неподходящий атрибут, вы запутаетесь в ваших pagedata.php!</p>";
-$plugin_tx['pagemanager']['cf_treeview_theme']="<p>Тема дерево виджета: <b>cmsimple</b></p><p>Другие темы доступны на сайте http://3-magi.net/?CMSimple_XH/Pagemanager_XH.</p>";
-$plugin_tx['pagemanager']['cf_treeview_animation']="<p>Продолжительность open/close анимации в ms. 0 означает отсутствие анимации.</p>";
-$plugin_tx['pagemanager']['cf_utf8_marker']="<p>Внутреннего применения. <strong>этого не изменить!</strong></p>";
-$plugin_tx['pagemanager']['menu_main']="Страница админа";
+/*
+ * Copyright (c) 2012 Old aka Lybomyr Kydray
+ *
+ * Translation are licensed under the GNU General Public License, version 2 or
+ * later.
+ */
+
+$plugin_tx['pagemanager']['cf_verbose']="<p>Показывать информацию и диалоги подтверждения? <b>true</b> или <b>false</b></p><p><strong><b>true</b> настоятельно рекомендуеиспользовать эту настройку, </strong>если вы не являетесь опытным пользователем <strong>и</strong> привыкли к Pagemanager_XH.</p>";
+$plugin_tx['pagemanager']['cf_toolbar_show']="<p>Всегда показывать панели инструментов? <b>true</b> или <b>false</b></p>";
+$plugin_tx['pagemanager']['cf_toolbar_vertical']="<p>Отображать панель инструментов вертикально? <b>true</b> or <b>false</b></p><p>Эта опция предназначена для опытных пользователей, которые могут настроить вид плагина на вкладке стилей плагина.</p>";
+$plugin_tx['pagemanager']['cf_pagedata_attribute']="<p>Да/нет (по умолчанию <b>да</b>) - установка состояния страниц с помощью поля выбора (\"галочки\"), например, <b>опубликована/published</b> или <b>связана с меню/linked_to_menu</b>.</p><p><strong>Внимание:</strong> если вы выбрали неверный атрибут, вы запутаетесь в ваших страницах (pagedata.php)!</p>";
+$plugin_tx['pagemanager']['cf_treeview_theme']="<p>Тема дерева менеджера страниц: <b>cmsimple</b></p><p>Другие темы доступны на сайте http://3-magi.net/?CMSimple_XH/Pagemanager_XH.</p>";
+$plugin_tx['pagemanager']['cf_treeview_animation']="<p>Продолжительность анимации закрытия/открытия окна в ms. Параметр 0 - без анимации.</p>";
+$plugin_tx['pagemanager']['cf_utf8_marker']="<p>Системная переменная. <strong>Не изменять!</strong></p>";
+$plugin_tx['pagemanager']['menu_main']="Страница администрирования";
 $plugin_tx['pagemanager']['op_expand']="Развернуть все страницы";
 $plugin_tx['pagemanager']['op_collapse']="Свернуть все страницы";
-$plugin_tx['pagemanager']['op_create']="Создать страницу (<em>в качестве суб-стр.</em>)";
-$plugin_tx['pagemanager']['op_create_after']="Создать страницу (<em>на этом же уровне, после суб-страниц</em>)";
+$plugin_tx['pagemanager']['op_create']="Создать страницу (<em>как зависимую страницу</em>)";
+$plugin_tx['pagemanager']['op_create_after']="Создать страницу (<em>на этом же уровне, после зависимых страниц</em>)";
 $plugin_tx['pagemanager']['op_rename']="Переименовать страницу";
-$plugin_tx['pagemanager']['op_delete']="Удалить страницу (<em>в том числе суб-страницу</em>)";
+$plugin_tx['pagemanager']['op_delete']="Удалить страницу (<em>зависимые страницы также будут удалены</em>)";
 $plugin_tx['pagemanager']['op_cut']="Вырезать";
 $plugin_tx['pagemanager']['op_copy']="Копировать";
-$plugin_tx['pagemanager']['op_paste']="Вставить (<em>в качестве суб-страницы</em>)";
-$plugin_tx['pagemanager']['op_paste_after']="Вставить (<em>на этом же уровне, после суб-страниц</em>)";
-$plugin_tx['pagemanager']['op_help']="<p>Вы можете изменить порядок страниц в <em>drag&amp;drop</em>. <em>Правой кнопкой мыши на странице</em> открывает контекстное меню. Более подробные инструкции доступны в плагине помощь.</p>";
+$plugin_tx['pagemanager']['op_paste']="Вставить (<em>как зависимую страницу</em>)";
+$plugin_tx['pagemanager']['op_paste_after']="Вставить (<em>на этом же уровне, после зависимых страниц</em>)";
+$plugin_tx['pagemanager']['op_help']="<p>Вы можете изменять порядок страниц <em>перетаскиванием</em>. <em>Нажатие правой кнопки мыши на названии страницы</em> открывает контекстное меню. Более подробные инструкции доступны в файле помощи для плагина.</p>";
 $plugin_tx['pagemanager']['treeview_loading']="Загрузка...";
 $plugin_tx['pagemanager']['treeview_new']="Новая страница";
 $plugin_tx['pagemanager']['button_delete']="Удалить";
-$plugin_tx['pagemanager']['button_ok']="OK";
-$plugin_tx['pagemanager']['button_cancel']="Закрыть";
+$plugin_tx['pagemanager']['button_ok']="Сохранить";
+$plugin_tx['pagemanager']['button_cancel']="Отмена";
 $plugin_tx['pagemanager']['message_confirm']="Конфигурация";
-$plugin_tx['pagemanager']['message_confirm_deletion']="Страница, где все суб-страницы будут удалены!";
-$plugin_tx['pagemanager']['message_warning_leave']="Ваши изменения будут утеряны!";
-$plugin_tx['pagemanager']['message_confirm_leave']="*** НЕСОХРАНЕННЫЕ ИЗМЕНЕНИЯ! ***\n\n Подтвердите, чтобы сохранить их, или они будут потеряны!\n";
+$plugin_tx['pagemanager']['message_confirm_deletion']="Все зависимые страницы будут удалены!";
+$plugin_tx['pagemanager']['message_warning_leave']="Ваши изменения будут потеряны!";
+$plugin_tx['pagemanager']['message_confirm_leave']="*** НЕСОХРАНЕННЫЕ ИЗМЕНЕНИЯ! ***\r\n\r\n Подтвердите, чтобы сохранить изменения, или они будут потеряны!\r\n";
 $plugin_tx['pagemanager']['message_information']="Информация";
-$plugin_tx['pagemanager']['message_menu_level']="Это может превышать ваш <em>меню уровня</em> настройки!";
+$plugin_tx['pagemanager']['message_menu_level']="Это может превышать возможности настройки ваших <em>уровней меню</em>!";
 $plugin_tx['pagemanager']['message_no_selection']="Вы должны выбрать страницу перед тем, как использовать этот инструмент!";
-$plugin_tx['pagemanager']['message_delete_last']="Вы не должны удалить последнею корневую страницу!";
-$plugin_tx['pagemanager']['error_phpversion']="Pagemanager_XH требует, по крайней мере, версия PHP %s!";
-$plugin_tx['pagemanager']['error_extension']="Pagemanager_XH требует расширение PHP %s !";
-$plugin_tx['pagemanager']['error_jquery']="Pagemanager_XH требует jQuery4CMSimple плагин! Пожалуйста, <a href='http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple'>скачайте</a> и установите его, прежде чем продолжить.";
-$plugin_tx['pagemanager']['error_utf8']="Pagemanager_XH требует Utf8_XH плагин! Пожалуйста, <a href='http://3-magi.net/?CMSimple_XH/Utf8_XH'>скачайте</a> и установите его, прежде чем продолжить.";
-$plugin_tx['pagemanager']['error_encoding']="Pagemanager_XH требует кодировку UTF-8!";
-$plugin_tx['pagemanager']['error_structure_confirmation']="Я знаю, что и <em>как</em> и то что я делаю! Я хочу, продолжить.";
-$plugin_tx['pagemanager']['error_structure_warning']="<strong>Нерегулярная структура страницы.</strong> подробнее см. плагин справка и обратитесь к веб-мастеру!";
-$plugin_tx['pagemanager']['error_nesting']="Слишком глубоко вложили структуру страницы!";
-$plugin_tx['pagemanager']['error_offending_extension']="Offending extension using its own jQuery library detected (see manual, section \"Limitations\").";
+$plugin_tx['pagemanager']['message_delete_last']="Вы не должны удалять верхнюю (корневую) страницу!";
+$plugin_tx['pagemanager']['error_phpversion']="Для использования Pagemanager_XH необходима версия PHP не ниже %s!";
+$plugin_tx['pagemanager']['error_extension']="Для использования Pagemanager_XH необходимо расширение PHP %s !";
+$plugin_tx['pagemanager']['error_jquery']="Для использования Pagemanager_XH необходим плагин jQuery4CMSimple! Пожалуйста,  прежде чем продолжить, <a href='http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple'>скачайте</a> и установите плагин jQuery4CMSimple.";
+$plugin_tx['pagemanager']['error_utf8']="Для использования менеджера страниц Pagemanager_XH необходим плагин Utf8_XH! Пожалуйста, <a href='http://3-magi.net/?CMSimple_XH/Utf8_XH'>скачайте</a> и установите этот плагин перед продолжением работы.";
+$plugin_tx['pagemanager']['error_encoding']="Для использования Pagemanager_XH необходима кодировка UTF-8!";
+$plugin_tx['pagemanager']['error_structure_confirmation']="Я уверен в том, <em>что</em> я делаю! Я хочу продолжить.";
+$plugin_tx['pagemanager']['error_structure_warning']="<strong>Неправильная структура страницы.</strong> Подробности в справке плагина. Советуем обратиться к разработчику вашего сайта!";
+$plugin_tx['pagemanager']['error_nesting']="Структура страниц имеет слишком большую вложенность!";
+$plugin_tx['pagemanager']['error_cant_rename']="Невозможно переименовать страницу в связи с тем, что ее заголовок частично отформатирован.";
+$plugin_tx['pagemanager']['error_offending_extension']="Обнаружено открытие расширения, использующего свою собственную библиотеку jQuery (см. руководство, секция \"Ограничения/Limitations\").";
 $plugin_tx['pagemanager']['utf8_marker']="äöüß";
 
 ?>
