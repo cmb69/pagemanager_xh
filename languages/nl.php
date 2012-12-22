f2a5cb4a9e9e93ecc0435e08f4f82b290a315373
cmb69 <cmbecker69@gmx.de>
2012-12-22 00:05:36 +0000

    added Dutch translation (contributed by roze)

---
+++ languages/nl.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Rob Zeijen
+ *
+ * This work is licensed under a Creative Commons Attribution 3.0 Unported
+ * License.
+ */
+
+$plugin_tx['pagemanager']['cf_verbose']="<p>Laat de informatie- en bevestiging meldingen zien? <b>true</b> or <b>false</b></p><p><strong><b>true</b> is sterk aanbevolen</strong>, behalve als u een zeer ervaren gebruiker bent<strong>en</strong> bekend bent met Pagemanager_XH.</p>";
+$plugin_tx['pagemanager']['cf_toolbar_show']="<p>Moet de werkbalk getoond worden? <b>true</b> or <b>false</b></p>";
+$plugin_tx['pagemanager']['cf_toolbar_vertical']="<p>Moet de werkbalk vertikaal getoond worden? <b>true</b> or <b>false</b></p><p>This option is for power users, who can adjust the layout in Plugin Stylesheet.</p>";
+$plugin_tx['pagemanager']['cf_pagedata_attribute']="<p>De ja/nee eigenschap van de paginagegevens, staan standaard op JA in de selectievakjes b.v.  <b>gepubliceerd</b> of <b>linked_to_menu</b>.</p><p><strong>Caution:</strong> If you choose an inappropriate attribute, you will mess up your pagedata.php!</p>";
+$plugin_tx['pagemanager']['cf_treeview_theme']="<p>Thema van de boomstructuur widget: <b>cmsimple</b></p><p>Andere thema's zijn beschikbaar zie http://3-magi.net/?CMSimple_XH/Pagemanager_XH.</p>";
+$plugin_tx['pagemanager']['cf_treeview_animation']="<p>tijdsduur van openen/sluiten van de animaties in ms. 0 betekent geen animatie.</p>";
+$plugin_tx['pagemanager']['cf_utf8_marker']="<p>Intern gebruik. <strong>Verander NIET!</strong></p>";
+$plugin_tx['pagemanager']['menu_main']="Beheer van de pagina";
+$plugin_tx['pagemanager']['op_expand']="Vouwt alle paginaniveau's uit";
+$plugin_tx['pagemanager']['op_collapse']="Laat alle pagina's samenvouwen";
+$plugin_tx['pagemanager']['op_create']="Maakt een pagina (<em>als een sub-pagina</em>)";
+$plugin_tx['pagemanager']['op_create_after']="Maakt een pagina (<em>op hetzelfde niveau- na de sub-pagina's</em>)";
+$plugin_tx['pagemanager']['op_rename']="Hernoem de pagina.";
+$plugin_tx['pagemanager']['op_delete']="Verwijder de pagina (<em>incl. sub-pagina's</em>)";
+$plugin_tx['pagemanager']['op_cut']="Knippen";
+$plugin_tx['pagemanager']['op_copy']="Kopieer";
+$plugin_tx['pagemanager']['op_paste']="Plakken (<em>als een sub-pagina</em>)";
+$plugin_tx['pagemanager']['op_paste_after']="Plakken (<em>op hetzelfde niveau - na de sub-pagina's</em>)";
+$plugin_tx['pagemanager']['op_help']="<p>U kunt de pagina's opnieuw ordenenen in een andere volgorde door te <em>slepen en weer los te laten</em> met de <strong>linker</strong> muisknop op die pagina. <br> <em>Met de <strong>rechter</strong> muisknop klikken op de pagina</em> opent het functie-menu voor die pagina. <br>Meer gedetailleerde instructies zijn beschikbaar in de plugin help.</p>";
+$plugin_tx['pagemanager']['treeview_loading']="Wordt geladen...";
+$plugin_tx['pagemanager']['treeview_new']="Nieuwe pagina";
+$plugin_tx['pagemanager']['button_delete']="Verwijderen";
+$plugin_tx['pagemanager']['button_ok']="OK";
+$plugin_tx['pagemanager']['button_cancel']="Annuleren";
+$plugin_tx['pagemanager']['message_confirm']="Bevestiging";
+$plugin_tx['pagemanager']['message_confirm_deletion']="Deze pagina en alle subpagina's worden verwijderd!";
+$plugin_tx['pagemanager']['message_warning_leave']="Uw wijzigingen zullen niet opgeslagen worden!";
+$plugin_tx['pagemanager']['message_confirm_leave']="*** NIET OPGESLAGEN VERANDERINGEN! ***\r\n\r\nBevestigen om ze op te slaan, anders bent u de veranderingen kwijt!\r\n";
+$plugin_tx['pagemanager']['message_information']="Informatie";
+$plugin_tx['pagemanager']['message_menu_level']="Dit zou de instellingen van <em>menu levels</em> overschrijden!";
+$plugin_tx['pagemanager']['message_no_selection']="U moet een pagina selecteren voordat u gebruik kunt maken van dit gereedschap!";
+$plugin_tx['pagemanager']['message_delete_last']="U moet de laatste beginpagina niet verwijderen!";
+$plugin_tx['pagemanager']['error_phpversion']="Pagemanager_XH vereist ten minste PHP versie %s!";
+$plugin_tx['pagemanager']['error_extension']="Pagemanager_XH requires PHP's %s extension!";
+$plugin_tx['pagemanager']['error_jquery']="Pagemanager_XH vereist de jQuery4CMSimple plugin!  <a href='http://www.cmsimple-xh.org/wiki/doku.php/extend:jquery4cmsimple'>download</a> en installeer eerst voordat u verder gaat!";
+$plugin_tx['pagemanager']['error_encoding']="Pagemanager_XH verlangt UTF-8 encoding!";
+$plugin_tx['pagemanager']['error_structure_confirmation']="Ik weet <em>precies</em> wat ik aan het doen ben!  Ik wil doorgaan.";
+$plugin_tx['pagemanager']['error_structure_warning']="<strong>Er is een onregelmatige paginastructuur gevonden.</strong> Voor details zie de plugin HELP en raadpleeg uw webmaster!";
+$plugin_tx['pagemanager']['error_nesting']="De paginastructuur is te ver onderverdeeld!";
+$plugin_tx['pagemanager']['error_cant_rename']="Deze pagina kan niet hernoemd worden omdat de titel gedeeltelijk opgemaakt is.";
+$plugin_tx['pagemanager']['error_offending_extension']="Er is een extensie die gebruik maakt van een eigen jQuery bibliotheek ontdekt (zie gebruiksaanwijzing, sectie \"Beperkingen\").";
+$plugin_tx['pagemanager']['utf8_marker']="äöüß";
+
+?>
