8ea72e11c4a6af0421de8e1d11778dc12b0e3045
cmb69 <cmbecker69@gmx.de>
2013-10-22 20:38:36 +0000

    replaced "cmsimple" theme with original themes

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ $plugin_cf['pagemanager']['verbose']="true";
 $plugin_cf['pagemanager']['toolbar_show']="true";
 $plugin_cf['pagemanager']['toolbar_vertical']="";
 $plugin_cf['pagemanager']['pagedata_attribute']="";
-$plugin_cf['pagemanager']['treeview_theme']="cmsimple";
+$plugin_cf['pagemanager']['treeview_theme']="default";
 $plugin_cf['pagemanager']['treeview_animation']="200";
 
 ?>
