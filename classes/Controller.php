5fdc6ab85794ec4c5d4aadc67c4a6319c030076c
cmb69 <cmbecker69@gmx.de>
2013-12-13 14:53:15 +0000

    fixed regression bug (r122), where the vertical toolbar called a non-existant function

--- classes/Controller.php
+++ classes/Controller.php
@@ -217,7 +217,7 @@ class Pagemanager_Controller
                 . '" target="_blank" id="' . $id . '" title="' . $tooltip . '"></a>';
         }
         if (!$horizontal) {
-            $o .= tab('br');
+            $o .= tag('br');
         }
         $o .= "\n";
         return $o;
