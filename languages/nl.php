9ea524aa1f24065eaee9c41a82bdcb98a58d07d4
cmb69 <cmbecker69@gmx.de>
2014-05-21 21:01:41 +0000

    added Dutch localization (contributed by bastingse)

---
+++ languages/nl.php
@@ -0,0 +1,51 @@
+<?php
+
+$plugin_tx['pagemanager']['menu_main']="Pagina beheren";
+$plugin_tx['pagemanager']['treeview_loading']="Laden...";
+$plugin_tx['pagemanager']['treeview_new']="Nieuwe pagina";
+$plugin_tx['pagemanager']['button_delete']="Verwijder";
+$plugin_tx['pagemanager']['button_ok']="OK";
+$plugin_tx['pagemanager']['button_cancel']="Anuleer";
+$plugin_tx['pagemanager']['button_save']="Opslaan";
+$plugin_tx['pagemanager']['op_expand']="Alle paginas uitklappen";
+$plugin_tx['pagemanager']['op_collapse']="Alle paginas inklappen";
+$plugin_tx['pagemanager']['op_create']="Maak pagina (als sub-pagina)";
+$plugin_tx['pagemanager']['op_create_after']="Maak pagina (op zelfde niveau)";
+$plugin_tx['pagemanager']['op_rename']="Hernoem pagina (pas interne links aan)";
+$plugin_tx['pagemanager']['op_delete']="Verwijder pagina (incl. sub-paginas)";
+$plugin_tx['pagemanager']['op_cut']="Knip pagina";
+$plugin_tx['pagemanager']['op_copy']="Kopieer pagina";
+$plugin_tx['pagemanager']['op_paste']="Plak pagina (als sub-pagina)";
+$plugin_tx['pagemanager']['op_paste_after']="Plak pagina (op zelfde niveau)";
+$plugin_tx['pagemanager']['op_help']="U kunt pagina volgorde aanpassen door te slepen. Klik met rechtermuis knop voor bewerkmenu. Engelstalige helppagina is beschikbaar door te klikken op dit icoon.";
+$plugin_tx['pagemanager']['message_confirm']="Bevestiging";
+$plugin_tx['pagemanager']['message_confirm_deletion']="De pagina met alle bijbehorende sub-pagina's worden verwijderd!";
+$plugin_tx['pagemanager']['message_warning_leave']="Uw aanpassingen gaan verloren!";
+$plugin_tx['pagemanager']['message_confirm_leave']="*** Niet opgeslagen aanpassingen! ***\n\nDoe eerst opslaan anders zullen aanpassingen verloren gaan!\n";
+$plugin_tx['pagemanager']['message_information']="Informatie";
+$plugin_tx['pagemanager']['message_menu_level']="Een subpagina is niet mogelijk. Meer menuniveaus zijn niet toegestaan!";
+$plugin_tx['pagemanager']['message_no_selection']="U dient eerst een pagina te selecteren alvorens deze functie te gebruiken!";
+$plugin_tx['pagemanager']['message_delete_last']="U kunt de laatste pagina niet verwijderen in dit systeem!";
+$plugin_tx['pagemanager']['message_save_success']="De pagina structuur is succesvol opgeslagen.";
+$plugin_tx['pagemanager']['message_save_failure']="De pagina structuur kon niet worden opgelsagen naar \"%s\"!";
+$plugin_tx['pagemanager']['error_structure_confirmation']="Doorgaan!";
+$plugin_tx['pagemanager']['error_structure_warning']="<strong>Foute pagina structuur gevonden.</strong> Voor details kijkop de helppagina van deze plugin of neem contact op met uw webmaster. Toch opslaan kan resulteren in  ongewenst resultaat omdat de pagina manager dit probleem zal proberen op te lossen.";
+$plugin_tx['pagemanager']['error_nesting']="Teveel niveaus in de pagina structuur!";
+$plugin_tx['pagemanager']['error_cant_rename']="Deze pagina kan niet worden hernoemt.";
+$plugin_tx['pagemanager']['error_offending_extension']="Offending extension using its own jQuery library detected (see manual, section \"Limitations\").";
+$plugin_tx['pagemanager']['syscheck_title']="System check";
+$plugin_tx['pagemanager']['syscheck_phpversion']="PHP version &ge; %s";
+$plugin_tx['pagemanager']['syscheck_extension']="Extension \"%s\" loaded";
+$plugin_tx['pagemanager']['syscheck_xhversion']="CMSimple_XH version &ge; %s";
+$plugin_tx['pagemanager']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.org/wiki/doku.php/extend:jquery4cmsimple\">jQuery4CMSimple plugin</a> installed";
+$plugin_tx['pagemanager']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['pagemanager']['syscheck_writable']="Folder \"%s\" writable";
+$plugin_tx['pagemanager']['about']="About Pagemanager_XH";
+$plugin_tx['pagemanager']['cf_verbose']="Whether to show information and confirmation dialogs.";
+$plugin_tx['pagemanager']['cf_toolbar_show']="Whether the toolbar shall be shown.";
+$plugin_tx['pagemanager']['cf_toolbar_vertical']="Whether the toolbar shall be displayed vertically. This option is for power users, who can adjust the layout in Plugin Stylesheet.";
+$plugin_tx['pagemanager']['cf_pagedata_attribute']="The pagedata attribute to be edited with the checkboxes: \"linked_to_menu\" (visible in menu) or \"published\" (published). Leave empty to hide the checkboxes.";
+$plugin_tx['pagemanager']['cf_treeview_theme']="Theme of tree view widget.";
+$plugin_tx['pagemanager']['cf_treeview_animation']="Duration of open/close animations in ms. 0 means no animation.";
+
+?>
