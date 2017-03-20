cmb69/pagemanager_xh

commit 361a1a4f3639cd23d696c8aeea97739cd97bb5f5

More privacy
We restrict the visibility of properties and methods to a minimum.
Mostly this affects properties and methods which have not been declared
private in the first place to get around the ugly `_` prefix required
by the PEAR coding standard.

classes/Model.php
@@ -25,21 +25,20 @@ class Model
 {
     /**
      * @var array
-     * @todo Make protected.
+     * @todo Make private.
      */
     public $headings;
 
     /**
      * @var array
-     * @todo Make protected.
      */
-    public $mayRename;
+    private $mayRename;
 
     /**
      * @param string $heading
      * @return string
      */
-    protected function mayRename($heading)
+    private function mayRename($heading)
     {
         return !preg_match('/<|&(?!(?:amp|quot|lt|gt);)/', $heading);
     }
@@ -48,7 +47,7 @@ class Model
      * @param string $heading
      * @return string
      */
-    protected function cleanedHeading($heading)
+    private function cleanedHeading($heading)
     {
         $heading = trim(strip_tags($heading));
         $heading = html_entity_decode($heading, ENT_COMPAT, 'UTF-8');

classes/Controller.php
@@ -27,9 +27,8 @@ class Controller
 {
     /**
      * @var object
-     * @todo Make protected.
      */
-    public $model;
+    private $model;
 
     public function __construct()
     {
@@ -40,7 +39,7 @@ class Controller
      * @param string $template
      * @return string
      */
-    protected function render($template)
+    private function render($template)
     {
         global $pth;
 
@@ -53,7 +52,7 @@ class Controller
     /**
      * @return array
      */
-    protected function systemChecks()
+    private function systemChecks()
     {
         global $pth, $plugin_tx;
 
@@ -89,7 +88,7 @@ class Controller
     /**
      * @return string
      */
-    protected function pluginIconPath()
+    private function pluginIconPath()
     {
         global $pth;
 
@@ -100,7 +99,7 @@ class Controller
      * @param string $state
      * @return string
      */
-    protected function stateIconPath($state)
+    private function stateIconPath($state)
     {
         global $pth;
 
@@ -110,7 +109,7 @@ class Controller
     /**
      * @return string
      */
-    protected function ajaxLoaderPath()
+    private function ajaxLoaderPath()
     {
         global $pth;
 
@@ -121,7 +120,7 @@ class Controller
      * @param string $key
      * @return string
      */
-    protected function lang($key)
+    private function lang($key)
     {
         global $plugin_tx;
 
@@ -132,7 +131,7 @@ class Controller
      * @param string $tool
      * @return string
      */
-    protected function tool($tool)
+    private function tool($tool)
     {
         global $pth, $plugin_cf, $plugin_tx;
 
@@ -162,7 +161,7 @@ class Controller
     /**
      * @return string
      */
-    protected function jsConfig()
+    private function jsConfig()
     {
         global $pth, $sn, $tx, $plugin_cf, $plugin_tx;
 
@@ -207,7 +206,7 @@ class Controller
     /**
      * @return string
      */
-    protected function toolbarClass()
+    private function toolbarClass()
     {
         global $plugin_cf;
 
@@ -218,7 +217,7 @@ class Controller
     /**
      * @return bool
      */
-    protected function hasToolbar()
+    private function hasToolbar()
     {
         global $plugin_cf;
 
@@ -228,7 +227,7 @@ class Controller
     /**
      * @return array
      */
-    protected function tools()
+    private function tools()
     {
         return array(
             'save', 'expand', 'collapse', 'create', 'create_after', 'rename',
@@ -239,7 +238,7 @@ class Controller
     /**
      * @return string
      */
-    protected function submissionURL()
+    private function submissionURL()
     {
         global $sn;
 
@@ -250,7 +249,7 @@ class Controller
     /**
      * @return string
      */
-    protected function jsScriptPath()
+    private function jsScriptPath()
     {
         global $pth;
 
@@ -260,7 +259,7 @@ class Controller
     /**
      * @return string
      */
-    protected function editView()
+    private function editView()
     {
         global $pth, $title, $plugin_tx;
 
@@ -275,7 +274,7 @@ class Controller
         return $this->render('widget');
     }
 
-    protected function save()
+    private function save()
     {
         global $pth, $plugin_tx, $_XH_csrfProtection;
 
@@ -334,7 +333,7 @@ class Controller
     /**
      * @return bool
      */
-    protected function isAdministrationRequested()
+    private function isAdministrationRequested()
     {
         global $pagemanager;
 
@@ -346,7 +345,7 @@ class Controller
     /**
      * @return string
      */
-    protected function renderInfoView()
+    private function renderInfoView()
     {
         global $title, $plugin_tx;
 

classes/JSONGenerator.php
@@ -28,12 +28,12 @@ class JSONGenerator
     /**
      * @var Model
      */
-    protected $model;
+    private $model;
 
     /**
      * @var Pages
      */
-    protected $pages;
+    private $pages;
 
     /**
      * @param Model $model
@@ -56,7 +56,7 @@ class JSONGenerator
      * @param ?int $parent
      * @return array
      */
-    protected function getPagesData($parent = null)
+    private function getPagesData($parent = null)
     {
         $res = array();
         $children = !isset($parent)
@@ -72,7 +72,7 @@ class JSONGenerator
      * @param int $index
      * @return array
      */
-    protected function getPageData($index)
+    private function getPageData($index)
     {
         global $plugin_cf, $pd_router;
 

classes/JSONProcessor.php
@@ -26,22 +26,22 @@ class JSONProcessor
     /**
      * @var array
      */
-    protected $contents;
+    private $contents;
 
     /**
      * @var array
      */
-    protected $newContents;
+    private $newContents;
 
     /**
      * @var array
      */
-    protected $pageData;
+    private $pageData;
 
     /**
      * @var int
      */
-    protected $level;
+    private $level;
 
     /**
      * @var int
@@ -51,22 +51,22 @@ class JSONProcessor
     /**
      * @var string
      */
-    protected $title;
+    private $title;
 
     /**
      * @var string
      */
-    protected $pdattrName;
+    private $pdattrName;
 
     /**
      * @var bool
      */
-    protected $pdattr;
+    private $pdattr;
 
     /**
      * @var bool
      */
-    protected $mayRename;
+    private $mayRename;
 
     /**
      * @param array $contents
@@ -92,7 +92,7 @@ class JSONProcessor
     /**
      * @param array $pages
      */
-    protected function processPages($pages)
+    private function processPages($pages)
     {
         $this->level++;
         foreach ($pages as $page) {
@@ -104,7 +104,7 @@ class JSONProcessor
     /**
      * @param array $page
      */
-    protected function processPage($page)
+    private function processPage($page)
     {
         $pattern = '/(copy_)?pagemanager-([0-9]*)/';
         $this->id = empty($page['attr']['id'])
@@ -125,7 +125,7 @@ class JSONProcessor
         $this->processPages($page['children']);
     }
 
-    protected function appendExistingPageContent()
+    private function appendExistingPageContent()
     {
         $content = $this->contents[$this->id];
         if ($this->mayRename) {
@@ -138,7 +138,7 @@ class JSONProcessor
      * @param string $content
      * @return string
      */
-    protected function replaceHeading($content)
+    private function replaceHeading($content)
     {
         $pattern = "/<!--XH_ml[0-9]:.*?-->/";
         $replacement = "<!--XH_ml{$this->level}:"
@@ -146,12 +146,12 @@ class JSONProcessor
         return preg_replace($pattern, $replacement, $content, 1);
     }
 
-    protected function appendNewPageContent()
+    private function appendNewPageContent()
     {
         $this->newContents[] = "<!--XH_ml{$this->level}:{$this->title}-->";
     }
 
-    protected function appendPageData()
+    private function appendPageData()
     {
         global $pd_router;
 

tests/attack/CSRFAttackTest.php
@@ -26,22 +26,24 @@
 
 namespace Pagemanager;
 
+use PHPUnit_Framework_TestCase;
+
 class CSRFAttackTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @var string
      */
-    protected $url;
+    private $url;
 
     /**
      * @var resource
      */
-    protected $curlHandle;
+    private $curlHandle;
 
     /**
      * @var string
      */
-    protected $cookieFile;
+    private $cookieFile;
 
     public function setUp()
     {
@@ -58,7 +60,7 @@ class CSRFAttackTest extends PHPUnit_Framework_TestCase
     /**
      * @param array $fields
      */
-    protected function setCurlOptions($fields)
+    private function setCurlOptions($fields)
     {
         $options = array(
             CURLOPT_POST => true,

tests/unit/JSONProcessorTest.php
@@ -31,7 +31,7 @@ class JSONProcessorTest extends PHPUnit_Framework_TestCase
      */
     private $parser;
 
-    protected function setUpPDRouterStub()
+    private function setUpPDRouterStub()
     {
         global $pd_router;
 

tests/unit/ModelTest.php
@@ -31,9 +31,9 @@ class ModelTest extends PHPUnit_Framework_TestCase
     /**
      * @var Model
      */
-    protected $model;
+    private $model;
 
-    public function setUp()
+    protected function setUp()
     {
         global $h, $cl, $l, $tx;
 

commit 341eb15d8d2f703606d8eae55031a272ae9694eb

Less privacy
We fix a regression introduced by commit 361a1a4.

classes/Model.php
@@ -32,7 +32,7 @@ class Model
     /**
      * @var bool[]
      */
-    private $mayRename;
+    public $mayRename;
 
     /**
      * @param string $heading