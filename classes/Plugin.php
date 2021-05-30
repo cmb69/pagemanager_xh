776e1fb63444b32414030a0d3a3321bb65fe7d5a
Christoph M. Becker <cmbecker69@gmx.de>
2021-05-30 13:33:32 +0200

    Remove unused variable

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -47,7 +47,7 @@ class Plugin
      */
     private function handleAdministration()
     {
-        global $plugin, $admin, $action;
+        global $admin, $action;
 
         $o = print_plugin_admin('on');
         switch ($admin) {
